5c3ceedab27ee666f5c593c126821e96a92b0899
thaptawon123 <iog011horizon@gmail.com>
2026-09-16 16:24:44 +0700

    ่้้่้่้gdg
    Refactored database connection to use environment variables and corrected logging message.

--- db.js
+++ db.js
@@ -1,14 +1,9 @@
 import { Sequelize, DataTypes } from "sequelize";
-// database connection
 import dotenv from "dotenv";
-dotenv.config();
-//const sequelize = new Sequelize("product_db", "dev_user", "dev_password", {
-// host: "localhost",
-//  port: 5439,
-// dialect: "postgres",
-// logging: false,
-//});
 
+dotenv.config();
+
+// database connection configuration
 const dbName = process.env.PGDATABASE;
 const dbUser = process.env.PGUSER;
 const dbPassword = process.env.PGPASSWORD;
@@ -27,6 +22,7 @@ const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
     },
   },
 });
+
 // define database schema
 const Product = sequelize.define("Product", {
   id: {
@@ -49,39 +45,11 @@ const connectDB = async () => {
     await sequelize.authenticate();
     console.log("Connected to PostgreSQL!!");
     await sequelize.sync({ alter: true });
-    console.log("Table syncronize !");
+    console.log("Table synchronize !");
   } catch (error) {
     console.error("Connection failed", error);
     process.exit(1);
   }
 };
-export { sequelize, Product, connectDB };
-// define database schema
-const Product = sequelize.define("Product", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  price: {
-    type: DataTypes.FLOAT,
-    allowNull: false,
-  },
-});
 
-const connectDB = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connected to PostgreSQL!!");
-    await sequelize.sync({ alter: true });
-    console.log("Table syncronize !");
-  } catch (error) {
-    console.error("Connection failed", error);
-    process.exit(1);
-  }
-};
 export { sequelize, Product, connectDB };
